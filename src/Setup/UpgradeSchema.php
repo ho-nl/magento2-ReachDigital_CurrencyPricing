f56c2ff975a18abcb0cecd6ac532a62ef8230608
Simon Prins <simon@reachdigital.nl>
2018-12-10 14:35:49 +0100

    [FIN2-285]: Replace unique key constraint on catalog_product_entity_tier_price table to include currency.

--- src/Setup/UpgradeSchema.php
+++ src/Setup/UpgradeSchema.php
@@ -20,12 +20,33 @@ class UpgradeSchema implements UpgradeSchemaInterface
     public function upgrade(SchemaSetupInterface $setup, ModuleContextInterface $context) :void
     {
         $setup->startSetup();
-        $setup->getConnection()->addColumn('catalog_product_entity_tier_price', 'currency', [
-            'type' => \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-            'comment'   => 'Currency for the price',
-            'length' => '3',
-            'nullable' => true
-        ]);
+        if (version_compare($context->getVersion(), '0.1', '<')) {
+
+            $setup->getConnection()->addColumn('catalog_product_entity_tier_price', 'currency', [
+                'type' => \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
+                'comment'   => 'Currency for the price',
+                'length' => '3',
+                'nullable' => true
+            ]);
+        }
+        if (version_compare($context->getVersion(), '0.2', '<')) {
+            $uniqueKeyName = $setup->getIdxName(
+                'catalog_product_entity_tier_price',
+                ['entity_id', 'all_groups', 'customer_group_id', 'qty', 'website_id'],
+                \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE
+            );
+            $setup->getConnection()->dropIndex('catalog_product_entity_tier_price', $uniqueKeyName);
+            $setup->getConnection()->addIndex(
+                'catalog_product_entity_tier_price',
+                $setup->getIdxName(
+                    'catalog_product_entity_tier_price',
+                    ['entity_id', 'all_groups', 'customer_group_id', 'qty', 'website_id', 'currency'],
+                    \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE
+                ),
+                ['entity_id', 'all_groups', 'customer_group_id', 'qty', 'website_id', 'currency'],
+                \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_UNIQUE
+            );
+        }
         $setup->endSetup();
     }
 }
