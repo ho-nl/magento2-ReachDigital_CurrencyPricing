fe09b56e113ff29f9628ecf4dc38b95174b4df64
Simon Prins <simon@reachdigital.nl>
2018-12-12 09:53:28 +0100

    [FIN2-285]: Use dropdown to select currency.

--- src/Ui/DataProvider/Product/Form/Modifier/AdvancedPricingCurrency.php
+++ src/Ui/DataProvider/Product/Form/Modifier/AdvancedPricingCurrency.php
@@ -6,20 +6,44 @@
 namespace ReachDigital\CurrencyPricing\Ui\DataProvider\Product\Form\Modifier;
 
 use Magento\Catalog\Ui\DataProvider\Product\Form\Modifier\AbstractModifier;
+use Magento\Ui\Component\Form\Element\Select;
 
 class AdvancedPricingCurrency extends AbstractModifier
 {
+
+    /**
+     * @var \Magento\Directory\Model\Currency
+     */
+    private $currencyModel;
+
+    /**
+     * AdvancedPricingCurrency constructor.
+     *
+     * @param \Magento\Directory\Model\Currency $currencyModel
+     */
+    public function __construct(
+        \Magento\Directory\Model\Currency $currencyModel
+    )
+    {
+        $this->currencyModel = $currencyModel;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function modifyMeta(array $meta)
     {
+        $currencyOptions = [['label' => 'Base currency', 'value' => null]];
+        foreach ($this->currencyModel->getConfigAllowCurrencies() as $value) {
+            $currencyOptions []= ['label' => $value, 'value' => $value];
+        }
         $meta['advanced_pricing_modal']['children']['advanced-pricing']['children']['tier_price']['children']['record']['children']['currency'] = [
             'arguments' => [
                 'data' => [
+                    'options' => $currencyOptions,
                     'config' => [
                         'dataType' => 'text',
-                        'formElement'=> 'input',
+                        'formElement'=> Select::NAME,
                         'componentType' => 'field',
                         'label' => __('Currency'),
                         'dataScope' => 'currency',
