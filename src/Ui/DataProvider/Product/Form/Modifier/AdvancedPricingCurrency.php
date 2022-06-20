5bbe549dd3246da02fe982ee68c49093d9c3dad9
Simon Prins <simon@reachdigital.nl>
2022-06-20 12:10:06 +0200

    [SLP-21]: Allow per storeview prices to be set for a product through the admin ui.

--- src/Ui/DataProvider/Product/Form/Modifier/AdvancedPricingCurrency.php
+++ src/Ui/DataProvider/Product/Form/Modifier/AdvancedPricingCurrency.php
@@ -30,6 +30,11 @@ class AdvancedPricingCurrency extends AbstractModifier
     private $currencyPriceResourceModel;
 
     /**
+     * @var \Magento\Store\Model\StoreManagerInterface
+     */
+    private $storeManager;
+
+    /**
      * AdvancedPricingCurrency constructor.
      *
      * @param LocatorInterface                                                $locator
@@ -39,11 +44,13 @@ class AdvancedPricingCurrency extends AbstractModifier
     public function __construct(
         LocatorInterface $locator,
         \Magento\Directory\Model\Currency $currencyModel,
-        CurrencyPrice $currencyPriceResourceModel
+        CurrencyPrice $currencyPriceResourceModel,
+        \Magento\Store\Model\StoreManagerInterface $storeManager
     ) {
         $this->currencyModel = $currencyModel;
         $this->locator = $locator;
         $this->currencyPriceResourceModel = $currencyPriceResourceModel;
+        $this->storeManager = $storeManager;
     }
 
     /**
@@ -51,9 +58,44 @@ class AdvancedPricingCurrency extends AbstractModifier
      */
     public function modifyMeta(array $meta)
     {
+        $currentStore = $this->storeManager->getStore();
+        $specificStoreScope = (int) $currentStore->getStoreId() !== 0;
+        $storeviewId = (int) $currentStore->getId() === 0 ? null : $currentStore->getId();
+        $productId = $this->locator->getProduct()->getId();
+        $currencyPriceObjects = $this->currencyPriceResourceModel->loadPriceData($productId, 'price', $storeviewId);
+        $specialCurrencyPriceObjects = $this->currencyPriceResourceModel->loadPriceData(
+            $productId,
+            'special',
+            $storeviewId
+        );
+        $currencyPricesSet = [];
+        foreach ($currencyPriceObjects as $currencyPriceObject) {
+            if ($currencyPriceObject['price'] !== 0) {
+                $currencyPricesSet[$currencyPriceObject['currency']] = true;
+            }
+        }
+        $specialCurrencyPricesSet = [];
+        foreach ($specialCurrencyPriceObjects as $currencyPriceObject) {
+            if ($currencyPriceObject['price'] !== 0) {
+                $specialCurrencyPricesSet[$currencyPriceObject['currency']] = true;
+            }
+        }
+
+        if (
+            $specificStoreScope &&
+            isset(
+                $meta['product-details']['children']['container_price']['children']['price']['arguments']['data'][
+                    'config'
+                ]['label']
+            )
+        ) {
+            $meta['product-details']['children']['container_price']['children']['price']['arguments']['data']['config'][
+                'data'
+            ]['label'] = __('Website Price');
+        }
         $currencyOptions = [['label' => 'Base currency', 'value' => null]];
-        foreach ($this->currencyModel->getConfigAllowCurrencies() as $value) {
-            $currencyOptions[] = ['label' => $value, 'value' => $value];
+        foreach ($this->currencyModel->getConfigAllowCurrencies() as $currency) {
+            $currencyOptions[] = ['label' => $currency, 'value' => $currency];
         }
         if (isset($meta['advanced_pricing_modal']['children']['advanced-pricing']['children']['tier_price'])) {
             $meta['advanced_pricing_modal']['children']['advanced-pricing']['children']['tier_price']['children'][
@@ -97,21 +139,33 @@ class AdvancedPricingCurrency extends AbstractModifier
         }
 
         $specialPriceCurrencies = [];
-        foreach ($this->currencyModel->getConfigAllowCurrencies() as $value) {
-            $specialPriceCurrencies[$value] = [
+        foreach ($this->currencyModel->getConfigAllowCurrencies() as $currency) {
+            $specialPriceCurrencies['special_price_currency_' . $currency] = [
                 'arguments' => [
                     'data' => [
                         'config' => [
                             'componentType' => 'field',
                             'formElement' => 'input',
                             'dataType' => 'price',
-                            'label' => $value,
+                            'label' => $currency,
                             'enableLable' => 'true',
-                            'dataScope' => 'special_price_currency.' . $value,
+                            'dataScope' => 'special_price_currency.' . $currency,
                         ],
                     ],
                 ],
             ];
+
+            if ($specificStoreScope) {
+                // Add 'Use Default' checkbox on storeview scope.
+                $specialPriceCurrencies['special_price_currency_' . $currency]['arguments']['data']['config'][
+                    'service'
+                ]['template'] = 'ui/form/element/helper/service';
+                if (!isset($specialCurrencyPricesSet[$currency])) {
+                    $specialPriceCurrencies['special_price_currency_' . $currency]['arguments']['data']['config'][
+                        'disabled'
+                    ] = 1;
+                }
+            }
         }
 
         $meta['advanced_pricing_modal']['children']['advanced-pricing']['children']['special_price_currency'] = [
@@ -132,21 +186,29 @@ class AdvancedPricingCurrency extends AbstractModifier
 
         $currencies = [];
 
-        foreach ($this->currencyModel->getConfigAllowCurrencies() as $value) {
-            $currencies[$value] = [
+        foreach ($this->currencyModel->getConfigAllowCurrencies() as $currency) {
+            $currencies['currency_price_' . $currency] = [
                 'arguments' => [
                     'data' => [
                         'config' => [
                             'componentType' => 'field',
                             'formElement' => 'input',
                             'dataType' => 'price',
-                            'label' => $value,
+                            'label' => $currency,
                             'enableLable' => 'true',
-                            'dataScope' => 'currency_price.' . $value,
+                            'dataScope' => 'currency_price.' . $currency,
                         ],
                     ],
                 ],
             ];
+            if ($specificStoreScope) {
+                // Add 'Use Default' checkbox on storeview scope.
+                $currencies['currency_price_' . $currency]['arguments']['data']['config']['service']['template'] =
+                    'ui/form/element/helper/service';
+                if (!isset($currencyPricesSet[$currency])) {
+                    $currencies['currency_price_' . $currency]['arguments']['data']['config']['disabled'] = 1;
+                }
+            }
         }
 
         $meta['product-details']['children']['container_currency_price'] = [
@@ -159,7 +221,7 @@ class AdvancedPricingCurrency extends AbstractModifier
                         'required' => '0',
                         'sortOrder' => 15,
                         'collapsible' => true,
-                        'opened' => false,
+                        'opened' => true,
                     ],
                 ],
             ],
@@ -177,16 +239,29 @@ class AdvancedPricingCurrency extends AbstractModifier
      */
     public function modifyData(array $data)
     {
+        $currentStore = $this->storeManager->getStore();
+        $storeviewId = (int) $currentStore->getId() === 0 ? null : $currentStore->getId();
         $productId = $this->locator->getProduct()->getId();
 
-        $currencyPriceObjects = $this->currencyPriceResourceModel->loadPriceData($productId, 'price');
-        $currencyPriceData = [];
+        $currencyPriceObjects = $this->currencyPriceResourceModel->loadPriceData($productId, 'price', $storeviewId);
         foreach ($currencyPriceObjects as $currencyPriceObject) {
             $currencyPriceData[$currencyPriceObject['currency']] =
                 (int) $currencyPriceObject['price'] === 0 ? '' : (string) $currencyPriceObject['price'];
         }
+        $defaultCurencyPriceObjects = $this->currencyPriceResourceModel->loadPriceData($productId, 'price', null);
+        foreach ($defaultCurencyPriceObjects as $currencyPriceObject) {
+            // The price was already set for the specific scope, so no need to set the default scope price.
+            if (
+                isset($currencyPriceData[$currencyPriceObject['currency']]) &&
+                $currencyPriceData[$currencyPriceObject['currency']] !== ''
+            ) {
+                continue;
+            }
+            $currencyPriceData[$currencyPriceObject['currency']] =
+                (int) $currencyPriceObject['price'] === 0 ? '' : (string) $currencyPriceObject['price'];
+        }
 
-        if (count($currencyPriceData) == 0) {
+        if (count($currencyPriceData) == 0 && count($defaultCurencyPriceObjects) == 0) {
             // set default to '' for currencies otherwise we cannot save
             foreach ($this->currencyModel->getConfigAllowCurrencies() as $value) {
                 $currencyPriceData[$value] = '';
@@ -195,14 +270,36 @@ class AdvancedPricingCurrency extends AbstractModifier
 
         $data[$productId][self::DATA_SOURCE_DEFAULT]['currency_price'] = $currencyPriceData;
 
-        $specialCurrencyPriceObjects = $this->currencyPriceResourceModel->loadPriceData($productId, 'special');
+        $specialCurrencyPriceObjects = $this->currencyPriceResourceModel->loadPriceData(
+            $productId,
+            'special',
+            $storeviewId
+        );
+
         $specialCurrencyPriceData = [];
         foreach ($specialCurrencyPriceObjects as $currencyPriceObject) {
             $specialCurrencyPriceData[$currencyPriceObject['currency']] =
                 (int) $currencyPriceObject['price'] === 0 ? '' : (string) $currencyPriceObject['price'];
         }
 
-        if (count($specialCurrencyPriceData) == 0) {
+        $defaultSpecialCurrencyPriceObjects = $this->currencyPriceResourceModel->loadPriceData(
+            $productId,
+            'special',
+            null
+        );
+        foreach ($defaultSpecialCurrencyPriceObjects as $currencyPriceObject) {
+            // The price was already set for the specific scope, so no need to set the default scope price.
+            if (
+                isset($specialCurrencyPriceData[$currencyPriceObject['currency']]) &&
+                $specialCurrencyPriceData[$currencyPriceObject['currency']] !== ''
+            ) {
+                continue;
+            }
+            $specialCurrencyPriceData[$currencyPriceObject['currency']] =
+                (int) $currencyPriceObject['price'] === 0 ? '' : (string) $currencyPriceObject['price'];
+        }
+
+        if (count($specialCurrencyPriceData) == 0 && count($defaultSpecialCurrencyPriceObjects) == 0) {
             // set default to '' for currencies otherwise we cannot save
             foreach ($this->currencyModel->getConfigAllowCurrencies() as $value) {
                 $specialCurrencyPriceData[$value] = '';
